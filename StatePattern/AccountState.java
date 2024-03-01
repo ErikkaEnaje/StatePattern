45b1be37c3b6b0ed05cb8e62ab65de84842d555f
Erikka Marielle M. Enaje <142382057+ErikkaEnaje@users.noreply.github.com>
2024-03-01 15:42:36 +0800

    Add files via upload

---
+++ StatePattern/AccountState.java
@@ -0,0 +1,9 @@
+package StateDesignPattern;
+
+public interface AccountState {
+    void deposit(double depositAmount);
+    void withdraw(double withdrawAmount);
+    void suspend();
+    void activate();
+    void close();
+}
---
+++ StatePattern/SuspendedState.java
@@ -0,0 +1,35 @@
+package StateDesignPattern;
+
+public class SuspendedState implements AccountState {
+    private final Account account;
+    public SuspendedState(Account account) {
+        this.account = account;
+    }
+
+    @Override
+    public void deposit(double depositAmount) {
+        System.out.println("Deposits are not allowed for suspended accounts!");
+    }
+
+    @Override
+    public void withdraw(double withdrawAmount) {
+        System.out.println("Withdrawals are not allowed; a suspension cannot be withdrawn..");
+    }
+
+    @Override
+    public void suspend() {
+        System.out.println("Account is already suspended!");
+    }
+
+    @Override
+    public void activate() {
+        account.setAccountState(new ActiveState(account));
+        System.out.println("Account is activated.");
+    }
+
+    @Override
+    public void close() {
+        account.setAccountState(new ClosedState(account));
+        System.out.println("Account is closed!.");
+    }
+}
